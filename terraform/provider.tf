2e633c6faefbbfec715d7f7d4d27e174f3914a70
mirceaconstantin434 <mirceaconstantin434@outlook.com>
2025-10-07 18:56:31 +0300

    Create first terrafrom plan

--- terraform/provider.tf
+++ terraform/provider.tf
@@ -1,9 +1,26 @@
+terraform {
+  required_providers {
+    azurerm = {
+      source = "hashicorp/azurerm"
+      version = "=4.0.0"
+    }
+  }
+}
+
 provider "azurerm" {
   features {}
 }
 
-resource "azurerm_resource_group" "example" {
-  name     = "example-resources"
+
+resource "azurerm_resource_group" "ascension_test_rg" {
+  name     = "ascesion-test-rg"
   location = "West Europe"
 }
 
+
+resource "azurerm_virtual_network" "azure_vnet_one" {
+  name                = "vnetone-start"
+  resource_group_name = azurerm_resource_group.ascension_test_rg.name
+  location            = azurerm_resource_group.ascension_test_rg.location
+  address_space       = ["10.0.0.0/16"]
+}

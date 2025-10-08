85c642b30f3294a7ee9480084fe529b32cdbe4ad
mirceaconstantin434 <mirceaconstantin434@outlook.com>
2025-10-08 23:11:25 +0300

    Build frontned app and deploy, and after cretae python fnction to deploy

--- terraform/modules/acr-registry/terraform.tf
+++ terraform/modules/acr-registry/terraform.tf
@@ -6,5 +6,10 @@ terraform {
       source  = "hashicorp/azurerm"
       version = "~> 3.0"
     }
+
+    random = {
+      source  = "hashicorp/random"
+      version = "~> 3.6"     # any 3.x compatible
+    }
   }
 }

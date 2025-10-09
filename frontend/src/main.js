228463249ca8e57f66d389fc3751689aa0471ee0
mirceaconstantin434 <mirceaconstantin434@outlook.com>
2025-10-09 20:34:54 +0300

    Resolve building image

---
+++ frontend/src/main.js
@@ -0,0 +1,8 @@
+import './style.css';
+
+document.querySelector('#app').innerHTML = `
+  <main>
+    <h1>Hello from Vite ✨</h1>
+    <p>If you can see this, your Docker build works.</p>
+  </main>
+`;
